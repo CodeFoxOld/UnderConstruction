70256b7de865cc4e58be47df6bac395c2251f64e
RainbowDashLabs <mail@code-fox.de>
2019-01-24 19:29:55 +0100

    refactoring add var for player objects

--- Assets/de.trustfallgames.targetsenior/character/Character.cs
+++ Assets/de.trustfallgames.targetsenior/character/Character.cs
@@ -1,21 +1,35 @@
-using de.trustfallgames.targetsenior.tilemap;
+﻿using de.trustfallgames.targetsenior.tilemap;
 using de.trustfallgames.targetsenior.util;
 using UnityEngine;
 
 namespace de.trustfallgames.targetsenior.character {
     [RequireComponent(typeof(Movement))]
-    public class Character {
-        private TileCoord _currentCoord;
-        private MoveDirection _moveDirection;
+    public class Character : MonoBehaviour {
+        [Header("Whole Player Object")]
+        [SerializeField] private Transform _player;
+        [Header("Character Model")]
+        [SerializeField] private Transform _character;
+        private TileCoord     _currentCoord;
+        private MoveDirection _moveDirection = MoveDirection.DownLeft;
 
         public TileCoord CurrentCoord {
-            get {return _currentCoord;}
-            set {_currentCoord = value;}
+            get { return _currentCoord; }
+            set { _currentCoord = value; }
         }
 
         public MoveDirection MoveDirection {
-            get {return _moveDirection;}
-            set {_moveDirection = value;}
+            get { return _moveDirection; }
+            set { _moveDirection = value; }
         }
+
+        public float GetCurrentRotation() {
+            return Movement.GetRotationValue(_moveDirection);
+        }
+
+        public MoveDirection CurrentMoveDirection { get { return _moveDirection; } set { _moveDirection = value; } }
+
+        public Transform CharacterTransform { get { return _character; } }
+
+        public Transform Player { get { return _player; } }
     }
 }
