fc5fb4b5b0f5cbf2736607e5a5a8bf991b3d3c22
RainbowDashLabs <mail@code-fox.de>
2019-01-24 19:31:44 +0100

    refactoring

--- Assets/de.trustfallgames.targetsenior/tilemap/MapManager.cs
+++ Assets/de.trustfallgames.targetsenior/tilemap/MapManager.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 using UnityEngine.Serialization;
 
 namespace de.trustfallgames.targetsenior.tilemap {
-
     public class MapManager : MonoBehaviour {
-        [Header("Use \"Generate Classes for Tiles\" to add scripts to all tiles and fill refresh values")] [SerializeField]
+        [Header("Use \"Generate Classes for Tiles\" to add scripts to all tiles and fill refresh values")]
+        [SerializeField]
         private string lastRefresh;
 
         [SerializeField] private Dictionary<TileCoord, Tile> _tiles;
@@ -17,27 +17,34 @@ namespace de.trustfallgames.targetsenior.tilemap {
             RefreshDictionary();
         }
 
-        void Update() {
-        }
+        void Update() { }
 
         /// <summary>
         /// Adds Tile Classes to Tiles. Adjust Position.
         /// </summary>
         [ContextMenu("Generate Classes for Tiles")]
         public void GenerateTileClasses() {
-            foreach(Transform child in transform) {
-                if(child.GetComponent<Tile>() == null) {
+            foreach (Transform child in transform) {
+                if (child.GetComponent<Tile>() == null) {
                     child.gameObject.AddComponent<Tile>().CheckCollider();
                 }
 
-                child.transform.transform.position = new Vector3((float)Math.Round(child.transform.position.x, MidpointRounding.ToEven), 0, (float)Math.Round(child.transform.position.z, MidpointRounding.ToEven));
-                child.gameObject.GetComponent<Tile>().SetTilecords((int)child.transform.position.x, (int)child.transform.position.z);
+                child.transform.transform.position = new Vector3(
+                                                                 (float) Math.Round(
+                                                                                    child.transform.position.x,
+                                                                                    MidpointRounding.ToEven), 0,
+                                                                 (float) Math.Round(
+                                                                                    child.transform.position.z,
+                                                                                    MidpointRounding.ToEven));
+                child.gameObject.GetComponent<Tile>()
+                     .SetTilecords((int) child.transform.position.x, (int) child.transform.position.z);
             }
 
             lastRefresh = DateTime.Now.Hour + ":" + DateTime.Now.Minute + ":" + DateTime.Now.Second;
         }
 
-        [Header("User \"Generate Field with Dimensions\" to generate a tilemap with the following dimensions")] [SerializeField]
+        [Header("User \"Generate Field with Dimensions\" to generate a tilemap with the following dimensions")]
+        [SerializeField]
         private int xDimension;
 
         [SerializeField] private int yDimension;
@@ -47,13 +54,14 @@ namespace de.trustfallgames.targetsenior.tilemap {
         /// </summary>
         [ContextMenu("Generate Tilemap with Dimensions")]
         public void GenerateTilemap() {
-            for(int i = 0; i < xDimension; i++) {
-                for(int j = 0; j < yDimension; j++) {
+            for (int i = 0; i < xDimension; i++) {
+                for (int j = 0; j < yDimension; j++) {
                     GameObject tile = GameObject.CreatePrimitive(PrimitiveType.Plane);
                     tile.transform.SetParent(transform);
                     tile.transform.localScale = new Vector3(0.1f, 1, 0.1f);
-                    tile.transform.position = new Vector3((0 - (xDimension / 2) + i), 0, (0 - (yDimension / 2) + j));
-                    tile.transform.name = "Tile: " + ((0 - (xDimension / 2)) + i) + "|" + ((0 - (yDimension / 2) + j));
+                    tile.transform.position   = new Vector3((0 - (xDimension / 2) + i), 0, (0 - (yDimension / 2) + j));
+                    tile.transform.name =
+                        "Tile: " + ((0 - (xDimension / 2)) + i) + "|" + ((0 - (yDimension / 2) + j));
                 }
             }
 
@@ -66,11 +74,11 @@ namespace de.trustfallgames.targetsenior.tilemap {
         [ContextMenu("Delete Tilemap")]
         public void DeleteTilemap() {
             List<GameObject> objects = new List<GameObject>();
-            foreach(Transform obj in transform) {
+            foreach (Transform obj in transform) {
                 objects.Add(obj.gameObject);
             }
 
-            while(objects.Count != 0) {
+            while (objects.Count != 0) {
                 DestroyImmediate(objects[0]);
                 objects.RemoveAt(0);
             }
@@ -81,10 +89,11 @@ namespace de.trustfallgames.targetsenior.tilemap {
         /// </summary>
         public void RefreshDictionary() {
             _tiles = new Dictionary<TileCoord, Tile>();
-            foreach(Transform obj in transform) {
+            foreach (Transform obj in transform) {
                 Tile tile = obj.GetComponent<Tile>();
                 _tiles[tile.Coords] = tile;
             }
+
             Debug.Log(_tiles.Count + " tiles refreshed");
         }
     }
