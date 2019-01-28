a265cab07b1f1923dd6bff8986a655643cf7567b
RainbowDashLabs <mail@code-fox.de>
2019-01-28 17:07:46 +0100

    added better mobile button

---
+++ Assets/de.trustfallgames.underConstruction/controlls/MobileButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using de.trustfallgames.underConstruction.character;
+using de.trustfallgames.underConstruction.util;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MobileButton : InteractableButton {
+    [SerializeField] private MoveDirection moveDirection;
+
+    private Controller _controller;
+    // Start is called before the first frame update
+    void Start() {
+        _controller = Character.getCharacter().Controller;
+    }
+
+    // Update is called once per frame
+    void Update() { }
+
+    public override void OnTouchStart() {
+        _controller.ButtonToggle(moveDirection, true);
+    }
+
+    public override void OnTouchEnd() {
+        _controller.ButtonToggle(moveDirection,false);
+    }
+}
