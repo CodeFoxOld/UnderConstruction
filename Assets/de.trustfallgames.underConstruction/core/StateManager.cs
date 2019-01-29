58244c0322409542f847181ea9f6b93e4ee30ec3
RainbowDashLabs <mail@code-fox.de>
2019-01-29 17:12:41 +0100

    refactoring

---
+++ Assets/de.trustfallgames.underConstruction/core/StateManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using de.trustfallgames.underConstruction.character;
+using de.trustfallgames.underConstruction.tilemap;
+using UnityEngine;
+
+public class StateManager : MonoBehaviour {
+    [SerializeField] private GameManager _gameManager;
+    [SerializeField] private MapManager  _mapManager;
+    [SerializeField] private Character   _character;
+
+    // Start is called before the first frame update
+    void Start() {
+        Instantiate(_gameManager);
+        Instantiate(_character);
+        Instantiate(_mapManager).GenerateTilemap();
+    }
+
+    // Update is called once per frame
+    void Update() { }
+}
