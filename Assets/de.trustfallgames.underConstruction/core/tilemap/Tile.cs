bed23519e94f571243acca2371df18f73c7d9770
RainbowDashLabs <mail@code-fox.de>
2019-01-30 16:59:54 +0100

    add spawn logic to tile

--- Assets/de.trustfallgames.underConstruction/core/tilemap/Tile.cs
+++ Assets/de.trustfallgames.underConstruction/core/tilemap/Tile.cs
@@ -1,38 +1,73 @@
-﻿using UnityEngine;
+﻿using System;
+using de.trustfallgames.underConstruction.util;
+using UnityEngine;
 using UnityEngine.Serialization;
 
 namespace de.trustfallgames.underConstruction.core.tilemap {
     [RequireComponent(typeof(BoxCollider))]
     [RequireComponent(typeof(MeshCollider))]
     public class Tile : MonoBehaviour {
-        
-        private GameObject house;
-        [SerializeField] private bool blocked;
+        private GameManager gameManager;
 
-        private Tile() {
-            
-        }
-        
+        private ObstacleData obstacleData;
+        private GameObject house;
+        private GameObject obstacleBlueprint;
+
+        [SerializeField] private bool blocked;
+        public bool Blocked => blocked;
+        public TileCoord Coords { get; set; }
+        private BoxCollider collider;
+
+        private Counter _spawnCounter;
+
+        private bool moving;
+
+        private Tile() { }
+
         public Tile(int x, int z) {
             Coords = new TileCoord(x, z);
         }
-        
-        private BoxCollider collider;
-
-        public void SetTilecords(int x, int z) {
-            Coords = new TileCoord(x,z);
-        }
-        
-        public TileCoord Coords { get; private set; }
-
 
         // Start is called before the first frame update
         void Start() {
-            
+            gameManager = GameManager.GetManager();
         }
 
         // Update is called once per frame
         void Update() {
+            if (_spawnCounter != null && _spawnCounter.Next()) {
+                SpawnObject();
+            }
+
+            if (moving) {
+                Move();
+            }
+        }
+
+        private void Move() {
+            gameObject.transform.Translate(0, 1 / (GameManager.GetManager().Settings.MoveUpSpeed * 60), 0);
+            if (Math.Abs(gameObject.transform.position.y) < 0.01) {
+                moving = false;
+                gameObject.transform.position = new Vector3(Coords.X, 0, Coords.Z);
+            }
+        }
+
+        private void SpawnObject() {
+            if (GameManager.GetManager().Character.CurrentCoord.Equals(Coords)
+            ) /*Player is on Field. concat new object on Player*/ {
+                //Concat object on Player
+            } else /*Player is not on Field. Create or Concat new object*/ {
+                var b = house;
+                house = Instantiate(obstacleBlueprint);
+                house.transform.position = new Vector3(Coords.X, -1, Coords.Z);
+                house.GetComponent<MeshFilter>().mesh = obstacleData.Mesh;
+                house.GetComponent<Material>().mainTexture = obstacleData.Material.mainTexture;
+                if (b != null) {
+                    b.transform.SetParent(house.transform);
+                }
+
+                moving = true;
+            }
         }
 
         private void OnValidate() {
@@ -42,15 +77,23 @@ namespace de.trustfallgames.underConstruction.core.tilemap {
 
         public void CheckCollider() {
             collider = GetComponent<BoxCollider>();
-            collider.size = new Vector3(10,5,10);
+            collider.size = new Vector3(10, 5, 10);
         }
 
-        public bool Blocked => blocked;
-
-        public void SpawnObject() {
-            //TODO:
+        public void InitialiseSpawnObject(ObstacleData obstacleData, GameObject obstacleBlueprint) {
+            this.obstacleBlueprint = obstacleBlueprint;
+            _spawnCounter = new Counter(gameManager.Settings.SpawnDuration, false);
+            var a = Instantiate(obstacleBlueprint);
+            if (this.obstacleData == null) {
+                this.obstacleData = obstacleData;
+            }
         }
-        
-        
+
+        public ObstacleData ObstacleData => obstacleData;
+    }
+
+    public enum ObstacleType {
+        House,
+        NotHouse
     }
 }
