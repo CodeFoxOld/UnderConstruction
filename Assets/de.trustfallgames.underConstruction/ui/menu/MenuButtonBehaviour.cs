20f7e5c2bf4b57d8d116cc9ee0882bbda0c9fa37
Freewing <Lennart.Burgold@gmail.com>
2019-01-30 17:59:29 +0100

    Gedöns hehe xd

---
+++ Assets/de.trustfallgames.underConstruction/ui/menu/MenuButtonBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonBehaviour : MonoBehaviour
+{
+    public void StartSceneLoad()
+    {
+        SceneManager.LoadScene("camera");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
