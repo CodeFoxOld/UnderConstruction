fdf959de0f86b2beae758d069d2cab6a6af30d2a
RainbowDashLabs <mail@code-fox.de>
2019-01-30 11:58:54 +0100

    implemented state manager

--- Assets/de.trustfallgames.underConstruction/core/StateManager.cs
+++ Assets/de.trustfallgames.underConstruction/core/StateManager.cs
@@ -3,17 +3,24 @@ using System.Collections.Generic;
 using de.trustfallgames.underConstruction.character;
 using de.trustfallgames.underConstruction.core.tilemap;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class StateManager : MonoBehaviour {
-    [SerializeField] private GameManager _gameManager;
-    [SerializeField] private MapManager  _mapManager;
-    [SerializeField] private Character   _character;
+    [SerializeField] private GameObject gameManager;
+    [SerializeField] private GameObject mapManager;
+    [SerializeField] private GameObject character;
+    [SerializeField] private GameObject ui;
+    [SerializeField] private GameObject spawnManager;
 
     // Start is called before the first frame update
     void Start() {
-        Instantiate(_gameManager);
-        Instantiate(_character);
-        Instantiate(_mapManager).GenerateTilemap();
+        Instantiate(gameManager);
+        Instantiate(mapManager).GetComponent<MapManager>().GenerateTilemap();
+        Instantiate(character);
+        Instantiate(ui);
+        Instantiate(spawnManager);
+        
+        Destroy(gameObject);
     }
 
     // Update is called once per frame
