f2ad06e7eec879103d3ae5063a8493ca06953ae2
RainbowDashLabs <mail@code-fox.de>
2019-01-30 16:59:42 +0100

    add obstacle data

---
+++ Assets/de.trustfallgames.underConstruction/core/spawnManager/ObstacleData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace de.trustfallgames.underConstruction.core.tilemap {
+    public class ObstacleData {
+        private int id;
+        private Material material;
+        private Mesh mesh;
+        private ObstacleType obstacleType;
+        
+        public ObstacleData(int id, Material material, Mesh mes, ObstacleType obstacleType) {
+            this.id = id;
+            this.material = material;
+            this.mesh = mesh;
+            this.obstacleType = obstacleType;
+        }
+
+        public int Id => id;
+        public Material Material => material;
+        public Mesh Mesh => mesh;
+
+        public ObstacleType ObstacleType => obstacleType;
+    }
+}
---
+++ Assets/de.trustfallgames.underConstruction/core/spawnManager/ObstacleStack.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using de.trustfallgames.underConstruction.core.tilemap;
+using UnityEngine;
+
+public class ObstacleStack : MonoBehaviour {
+    [SerializeField] private ObstacleType obstacleType;
+    [SerializeField] private Material[] materials;
+    [SerializeField] private Mesh mesh;
+
+    public ObstacleType ObstacleType { get => obstacleType; set => obstacleType = value; }
+    public Material[] Materials => materials;
+    public Mesh Mesh => mesh;
+}
