b8a82d73c56e6fa7b5a752689cdb2d1dbbc06a16
Freewing <Lennart.Burgold@gmail.com>
2019-02-01 15:19:28 +0100

    Implemented Pause Menu Canvas

--- Assets/de.TrustfallGames.UnderConstruction/UI/UiManager.cs
+++ Assets/de.TrustfallGames.UnderConstruction/UI/UiManager.cs
@@ -2,15 +2,37 @@ using de.TrustfallGames.UnderConstruction.Core.CoreManager;
 using UnityEngine;
 
 namespace de.TrustfallGames.UnderConstruction.UI {
-    public class UiManager :MonoBehaviour{
+    public class UiManager :MonoBehaviour
+    {
+        [SerializeField] private GameObject pauseMenuCanvas;
+        [SerializeField] private GameObject ui;
+        
         private GameManager _gamemanager;
         private bool gamePaused;
         public bool GamePaused => gamePaused;
+        
+        private void Start()
+        {
+            _gamemanager= GameManager.GetManager().RegisterUiManager(this);
+            
+            pauseMenuCanvas = Instantiate(pauseMenuCanvas);
+            ui = Instantiate(ui);
+            
+            pauseMenuCanvas.SetActive(false);
+        }
 
-        private void Start() { _gamemanager= GameManager.GetManager().RegisterUiManager(this); }
+        public void OnGamePaused()
+        {
+            gamePaused = true;
+            ui.SetActive(false);
+            pauseMenuCanvas.SetActive(true);
+        }
 
-        public void OnGamePaused() { gamePaused = true; }
-
-        public void OnGameContinue() { gamePaused = false; }
+        public void OnGameContinue()
+        {
+            gamePaused = false;
+            ui.SetActive(true);
+            pauseMenuCanvas.SetActive(false);
+        }
     }
 }
--- Assets/de.trustfallgames.underConstruction/core/StateManager.cs
+++ Assets/de.trustfallgames.underConstruction/core/StateManager.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 public class StateManager : MonoBehaviour {
     [SerializeField] private GameObject gameManager;
     [SerializeField] private GameObject mapManager;
-    [SerializeField] private GameObject character;
-    [SerializeField] private GameObject ui;
+    [SerializeField] private GameObject character;    
     [SerializeField] private GameObject spawnManager;
 
     // Start is called before the first frame update
@@ -13,13 +12,9 @@ public class StateManager : MonoBehaviour {
         Instantiate(gameManager);
         Instantiate(mapManager).GetComponent<MapManager>().GenerateTilemap();
         Instantiate(character);
-        Instantiate(ui);
         if (spawnManager != null)
             Instantiate(spawnManager);
-
+        
         Destroy(gameObject);
     }
-
-    // Update is called once per frame
-    void Update() { }
 }
