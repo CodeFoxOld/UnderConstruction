5ff76e93757c9bdd9b26a5e182bc927950df5393
RDLabs <mail@code-fox.de>
2019-02-18 16:30:36 +0100

    implemented title screen

---
+++ Assets/de.TrustfallGames.UnderConstruction/Util/FadeThisOut.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using de.TrustfallGames.UnderConstruction.UI.title_screen;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace de.TrustfallGames.UnderConstruction.Util {
+    public class FadeThisOut : MonoBehaviour {
+        private Image image;
+
+        [Range(0.01f, 1)]
+        [SerializeField]
+        private float fadeDuration;
+
+        // Start is called before the first frame update
+        void Start() { image = GetComponent<Image>(); }
+
+        // Update is called once per frame
+        void Update() {
+            TitleBehaviour.FadeAlpha(ref image, fadeDuration);
+            if (Math.Abs(image.color.a) < 0.001) {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
