69ed140085791830ec3687cc98b81b9a54a19253
RDLabs <mail@code-fox.de>
2019-02-20 15:48:37 +0100

    ui implemention

--- Assets/SceneChanger.cs
+++ Assets/SceneChanger.cs
@@ -4,18 +4,38 @@ using de.TrustfallGames.UnderConstruction.GameTimeManager;
 using de.TrustfallGames.UnderConstruction.Util;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneChanger : MonoBehaviour {
     [SerializeField] private SceneEnum sceneEnum;
-    [SerializeField] public bool LoadAsync;
+    [SerializeField] private bool LoadAsync;
+    [SerializeField] private bool FadeOver;
+    [SerializeField] private Image FadeImage;
 
+    private void Awake() {
+        if (FadeImage != null) {
+            FadeImage.enabled = false;
+        }
+    }
+
+    private void FixedUpdate() { }
+
+    
+    
     public void ChangeScene() {
+        if (FadeOver) {
+            StartFade();
+            return;
+        }
+        
         if (LoadAsync) {
             SceneManager.LoadSceneAsync((int) sceneEnum);
         } else {
             SceneManager.LoadScene((int) sceneEnum);
         }
     }
+
+    private void StartFade() { throw new System.NotImplementedException(); }
 
     public void QuitApplication() { Application.Quit(); }
 
