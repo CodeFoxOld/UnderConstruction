4d8402729fd5c92256d2167aa65b3e5ab05a8286
RainbowDashLabs <mail@code-fox.de>
2019-04-06 02:29:35 +0200

    add Destructible Score calculation

--- Assets/de.trustfallgames.underConstruction/character/Character.cs
+++ Assets/de.trustfallgames.underConstruction/character/Character.cs
@@ -22,25 +22,25 @@ namespace de.TrustfallGames.UnderConstruction.Character {
         [SerializeField]
         private GameObject apartmentBlueprint;
 
-        private TileCoord _currentCoord = new TileCoord(0, 0);
-        private MoveDirection _moveDirection = MoveDirection.left;
-        private Controller _controller;
-        [SerializeField] private Movement _movement;
-        private bool moving;
-        private int height;
+        private                  TileCoord     _currentCoord  = new TileCoord(0, 0);
+        private                  MoveDirection _moveDirection = MoveDirection.left;
+        private                  Controller    _controller;
+        [SerializeField] private Movement      _movement;
+        private                  bool          moving;
+        private                  int           height;
 
         public bool Moving => moving;
 
-        private int colorCount = 1;
+        private int                colorCount      = 1;
         private ApartmentColorType latestColorType = ApartmentColorType.None;
 
         //UI Stuff
-        [SerializeField] private int highscore;
-        private int highscoreRest;
-        private int destRest;
-        private GameManager gameManager;
-        [SerializeField] public int destructibleCount;
-        public int Highscore => highscore;
+        [SerializeField] private int         highscore;
+        private                  int         highscoreRest;
+        private                  int         destRest;
+        private                  GameManager gameManager;
+        [SerializeField] public  int         destructibleCount;
+        public                   int         Highscore => highscore;
 
         public int DestructibleCount => destructibleCount;
 
@@ -49,40 +49,52 @@ namespace de.TrustfallGames.UnderConstruction.Character {
         private void Start() {
             gameManager = GameManager.GetManager().RegisterCharacter(this);
             _controller = gameManager.Controller;
-            _movement = GetComponent<Movement>();
+            _movement   = GetComponent<Movement>();
             RegisterInternUpdate();
         }
 
         private void Move() {
             _character.transform.Translate(0, 1 / (GameManager.GetManager().Settings.MoveUpSpeed * 60), 0);
             if (Math.Abs(_character.transform.position.y) < 0.01) {
-                moving = false;
+                moving                        = false;
                 _character.transform.position = new Vector3(CurrentCoord.X, 0, CurrentCoord.Z);
             }
         }
 
         public void Stack(ApartmentPart apartmentPart) {
-            SoundHandler.GetInstance().PlaySound(SoundName.CharacterPickup, false,GetInstanceID());
+            SoundHandler.GetInstance().PlaySound(SoundName.CharacterPickup, false, GetInstanceID());
             var b = _character.gameObject.transform;
-            _character = Instantiate(apartmentBlueprint).transform;                    //Create Blueprint
-            _character.position = new Vector3(CurrentCoord.X, -1, CurrentCoord.Z);     //Assign under tile
-            _character.GetComponent<MeshFilter>().mesh = apartmentPart.Mesh;           //Assign mesh
-            _character.GetComponent<MeshRenderer>().material = apartmentPart.Material; //Assign material
+            _character =
+                Instantiate(apartmentBlueprint).transform; //Create Blueprint
+            _character.position =
+                new Vector3(CurrentCoord.X, -1, CurrentCoord.Z); //Assign under tile
+            _character.GetComponent<MeshFilter>().mesh =
+                apartmentPart.Mesh; //Assign mesh
+            _character.GetComponent<MeshRenderer>().material =
+                apartmentPart.Material; //Assign material
             _character.localRotation = b.transform.localRotation;
             _character.SetParent(_player);
             b.SetParent(_character); //set old parent as Child
-            b.position = new Vector3(_character.position.x, 0, _character.position.z);
-            moving = true;           //Start moving
+            b.position              = new Vector3(_character.position.x, 0, _character.position.z);
+            moving                  = true; //Start moving
             _movement.CharTransform = _character.transform;
             CalculateHighscore(apartmentPart);
             height++;
+        }
+
+        public int CalculateDestructibleScore(int combo) {
+            int pointsAdded = gameManager.Settings.BasePoint * combo;
+            highscore += pointsAdded;
+            gameManager.UiManager.ChangeHighscore(highscore);
+
+            return pointsAdded;
         }
 
         private void CalculateHighscore(ApartmentPart apartmentPart) {
             if (latestColorType == apartmentPart.ApartmentColorType) {
                 colorCount++;
             } else {
-                colorCount = 1;
+                colorCount      = 1;
                 latestColorType = apartmentPart.ApartmentColorType;
             }
 
@@ -95,8 +107,8 @@ namespace de.TrustfallGames.UnderConstruction.Character {
             destructibleCount += dest > gameManager.Settings.MaxDestructablesPerCalc ?
                                      gameManager.Settings.MaxDestructablesPerCalc : dest;
 
-            gameManager.UiManager.OnHighscoreCalc(colorCount, apartmentPart.ApartmentColorType, highscore, height
-            ).OnDeconstructorChange(DestructibleCount);
+            gameManager.UiManager.OnHighscoreCalc(colorCount, apartmentPart.ApartmentColorType, highscore, height)
+                       .OnDeconstructorChange(DestructibleCount);
         }
 
         /// <summary>
@@ -120,12 +132,12 @@ namespace de.TrustfallGames.UnderConstruction.Character {
             }
         }
 
-        public TileCoord CurrentCoord { get { return _currentCoord; } set { _currentCoord = value; } }
-        public Transform CharacterTransform { get { return _character; } }
+        public TileCoord     CurrentCoord         { get { return _currentCoord; } set { _currentCoord = value; } }
+        public Transform     CharacterTransform   { get { return _character; } }
         public MoveDirection CurrentMoveDirection { get { return _moveDirection; } set { _moveDirection = value; } }
-        public MoveDirection MoveDirection { get { return _moveDirection; } set { _moveDirection = value; } }
-        public Transform Player { get { return _player; } }
-        public Controller Controller { get { return _controller; } }
+        public MoveDirection MoveDirection        { get { return _moveDirection; } set { _moveDirection = value; } }
+        public Transform     Player               { get { return _player; } }
+        public Controller    Controller           { get { return _controller; } }
 
         public ApartmentColorType LatestColorType => latestColorType;
 
@@ -133,8 +145,6 @@ namespace de.TrustfallGames.UnderConstruction.Character {
             if (moving) {
                 Move();
             }
-            
-            
         }
 
         public void OnDestroy() {
